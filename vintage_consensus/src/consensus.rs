fc5ecaa2b181021c819be468776525a927b59f0f
Hacken <taokeqin@gmail.com>
2024-08-08 18:08:42 +0800

    compile run function

--- vintage_consensus/src/consensus.rs
+++ vintage_consensus/src/consensus.rs
@@ -22,8 +22,7 @@ use overlord::error::ConsensusError;
 use overlord::types::{Commit, Hash, Node, OverlordMsg, Status, ViewChangeReason};
 use overlord::{Codec, Consensus, Crypto, DurationConfig, Overlord, OverlordHandler, Wal};
 use vintage_msg::Block;
-use tokio::sync::mpsc::{Sender, Receiver};
-
+use tokio::sync::{mpsc};
 lazy_static! {
     static ref HASHER_INST: HasherKeccak = HasherKeccak::new();
 }
@@ -31,31 +30,6 @@ lazy_static! {
 const SPEAKER_NUM: u8 = 10;
 
 const SPEECH_INTERVAL: u64 = 1000; // ms
-
-type Channel = (Sender<OverlordMsg<ConsensusBlock>>, Receiver<OverlordMsg<ConsensusBlock>>);
-#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
-
-pub struct ConsensusBlock(Block);
-macro_rules! impl_codec_for {
-    ($($struc: ident),+) => {
-        $(
-            impl Codec for $struc {
-                fn encode(&self) -> Result<Bytes, Box<dyn Error + Send>> {
-                    Ok(Bytes::from(bincode::serialize(&self).unwrap()))
-                    // serialize block to bytes
-                }
-
-                fn decode(data: Bytes) -> Result<Self, Box<dyn Error + Send>> {
-                    // deserialize bytes to block
-                    let data: Self = bincode::deserialize(&data).unwrap();
-                    Ok(data)
-                }
-            }
-        )+
-    }
-}
-
-impl_codec_for!(ConsensusBlock);
 
 struct MockWal {
     inner: Mutex<Option<Bytes>>,
@@ -129,31 +103,28 @@ impl Crypto for MockCrypto {
 
 struct ConsensusEngine {
     peer_list: Vec<Node>,
-    outbound: Sender<NetworkMsg>,
-    inbound: Receiver<OverlordMsg<Block>>,
+    outbound: mpsc::Sender<NetworkMsg>,
 }
 
 impl ConsensusEngine {
     fn new(
         peer_list: Vec<Node>,
-        outbound: Sender<NetworkMsg>,
-        inbound: Receiver<OverlordMsg<Block>>,
+        outbound: mpsc::Sender<NetworkMsg>,
     ) -> ConsensusEngine {
         ConsensusEngine {
             peer_list,
-            outbound,
-            inbound
+            outbound
         }
     }
 }
 
 #[async_trait]
-impl Consensus<ConsensusBlock> for ConsensusEngine {
+impl Consensus<Block> for ConsensusEngine {
     async fn get_block(
         &self,
         _ctx: Context,
         _height: u64,
-    ) -> Result<(ConsensusBlock, Hash), Box<dyn Error + Send>> {
+    ) -> Result<(Block, Hash), Box<dyn Error + Send>> {
         let header = BlockHeader{
             height:1,
             hash:[0; 32],
@@ -164,7 +135,7 @@ impl Consensus<ConsensusBlock> for ConsensusEngine {
             acts:vec![]
         };
         let block = Block{header, body};
-        Ok((ConsensusBlock(block),"".into()))
+        Ok((block,"".into()))
         // TODO:
         // send BlockProduce
         // wait response and return
@@ -175,7 +146,7 @@ impl Consensus<ConsensusBlock> for ConsensusEngine {
         _ctx: Context,
         _height: u64,
         _hash: Hash,
-        _speech: ConsensusBlock,
+        _speech: Block,
     ) -> Result<(), Box<dyn Error + Send>> {
         Ok(())
     }
@@ -184,7 +155,7 @@ impl Consensus<ConsensusBlock> for ConsensusEngine {
         &self,
         _ctx: Context,
         height: u64,
-        commit: Commit<ConsensusBlock>,
+        commit: Commit<Block>,
     ) -> Result<Status, Box<dyn Error + Send>> {
         Ok(Status {
             height: height + 1,
@@ -205,7 +176,7 @@ impl Consensus<ConsensusBlock> for ConsensusEngine {
     async fn broadcast_to_other(
         &self,
         _ctx: Context,
-        words: OverlordMsg<ConsensusBlock>,
+        words: OverlordMsg<Block>,
     ) -> Result<(), Box<dyn Error + Send>> {
         //Skip for now
         Ok(())
@@ -216,7 +187,7 @@ impl Consensus<ConsensusBlock> for ConsensusEngine {
         &self,
         _ctx: Context,
         name: Bytes,
-        words: OverlordMsg<ConsensusBlock>,
+        words: OverlordMsg<Block>,
     ) -> Result<(), Box<dyn Error + Send>> {
         //Skip for now
         Ok(())
@@ -235,23 +206,23 @@ impl Consensus<ConsensusBlock> for ConsensusEngine {
 }
 
 pub struct Validator {
-    overlord: Arc<Overlord<ConsensusBlock, ConsensusEngine, MockCrypto, MockWal>>,
-    handler: OverlordHandler<ConsensusBlock>,
+    overlord: Arc<Overlord<Block, ConsensusEngine, MockCrypto, MockWal>>,
+    handler: OverlordHandler<Block>,
     consensus_engine: Arc<ConsensusEngine>,
+    inbound: tokio::sync::Mutex<mpsc::Receiver<OverlordMsg<Block>>>
 }
 
 impl Validator {
     pub fn new(
         name: Bytes,
         node_list: Vec<Node>,
-        outbound: Sender<NetworkMsg>,
-        inbound: Receiver<OverlordMsg<Block>> //this is our block chian or database.
+        outbound: mpsc::Sender<NetworkMsg>,
+        inbound: mpsc::Receiver<OverlordMsg<Block>> //this is our block chian or database.
     ) -> Self {
         let crypto = MockCrypto::new(name.clone());
         let consensus_engine = Arc::new(ConsensusEngine::new(
             node_list.clone(),
-            outbound,
-            inbound
+            outbound
         ));
         let overlord = Overlord::new(
             name,
@@ -277,47 +248,61 @@ impl Validator {
             overlord: Arc::new(overlord),
             handler: overlord_handler,
             consensus_engine,
+            inbound: tokio::sync::Mutex::new(inbound)
         }
     }
 
     async fn run(
-        &self,
+        self: Arc<Self>,
         interval: u64,
         timer_config: Option<DurationConfig>,
         node_list: Vec<Node>,
     ) -> Result<(), Box<dyn Error + Send>> {
         let brain = Arc::<ConsensusEngine>::clone(&self.consensus_engine);
         let handler = self.handler.clone();
-        /* 
-        thread::spawn(move || loop {
-            if let Ok(msg) = brain.inbound.recv() {
-                match msg {
-                    OverlordMsg::SignedVote(vote) => {
-                        handler
-                            .send_msg(Context::new(), OverlordMsg::SignedVote(vote))
-                            .unwrap();
+        let s = self.clone();
+        tokio::spawn(async move 
+            { 
+                loop {
+                    let msg = {
+                        let mut receiver = s.inbound.lock().await;
+                        receiver.recv().await
+                    };
+            match msg {
+                Some(msg) => {
+                    match msg {
+                        OverlordMsg::SignedVote(vote) => {
+                            handler
+                                .send_msg(Context::new(), OverlordMsg::SignedVote(vote))
+                                .unwrap();
+                        }
+                        OverlordMsg::SignedProposal(proposal) => {
+                            handler
+                                .send_msg(Context::new(), OverlordMsg::SignedProposal(proposal))
+                                .unwrap();
+                        }
+                        OverlordMsg::AggregatedVote(agg_vote) => {
+                            handler
+                                .send_msg(Context::new(), OverlordMsg::AggregatedVote(agg_vote))
+                                .unwrap();
+                        }
+                        OverlordMsg::SignedChoke(choke) => {
+                            handler
+                                .send_msg(Context::new(), OverlordMsg::SignedChoke(choke))
+                                .unwrap();
+                        }
+                        _ => {}
                     }
-                    OverlordMsg::SignedProposal(proposal) => {
-                        handler
-                            .send_msg(Context::new(), OverlordMsg::SignedProposal(proposal))
-                            .unwrap();
-                    }
-                    OverlordMsg::AggregatedVote(agg_vote) => {
-                        handler
-                            .send_msg(Context::new(), OverlordMsg::AggregatedVote(agg_vote))
-                            .unwrap();
-                    }
-                    OverlordMsg::SignedChoke(choke) => {
-                        handler
-                            .send_msg(Context::new(), OverlordMsg::SignedChoke(choke))
-                            .unwrap();
-                    }
-                    _ => {}
+                }
+                None => {
+                    eprintln!("receive nothing");
+                    // Depending on the error type, you might want to break the loop here
+                    // break;
                 }
             }
-        });
-        */
-        self.overlord
+        }});
+        
+        self.clone().overlord
             .run(0, interval, node_list, timer_config)
             .await
             .unwrap();
