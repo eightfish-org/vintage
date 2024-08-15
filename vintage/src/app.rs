96abc4e1f66788048c25a22743b6f9e466d4b00f
Daogang Tang <daogangtang@gmail.com>
2024-08-15 12:49:37 -0700

    fix a bug when bootstrap from zero.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- vintage/src/app.rs
+++ vintage/src/app.rs
@@ -3,7 +3,8 @@ use tokio::task::JoinHandle;
 use vintage_blockchain::{BlockChain, BlockChainApiImpl, TxService};
 use vintage_consensus::Validator;
 use vintage_msg::{
-    BlockChainMsgChannels, ConsensusMsgChannels, NetworkMsgChannels, ProxyMsgChannels,BlockChainApi
+    BlockChainApi, BlockChainMsgChannels, ConsensusMsgChannels, NetworkMsgChannels,
+    ProxyMsgChannels,
 };
 use vintage_network::{config::NodeConfig, Node};
 use vintage_proxy::Proxy;
@@ -35,7 +36,8 @@ impl Vintage {
         )
         .await?;
         let mut block_height = blockchain_api.get_block_height().await.unwrap();
-        block_height = if block_height == 0 { 0 } else { block_height + 1 };
+        // block_height = if block_height == 0 { 0 } else { block_height + 1 };
+        block_height += 1;
         let node = Node::create(&config, network_chn, consensus_chn.consensus_msg_sender).await?;
         let validator = Validator::new(
             &config,
@@ -43,7 +45,7 @@ impl Vintage {
             consensus_chn.msg_receiver,
             blockchain,
             blockchain_api,
-            block_height
+            block_height,
         );
 
         Ok(Self {
