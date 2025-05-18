1b5f4ea9c6d719e558efc068c1cfd010f07baa64
Daogang Tang <daogangtang@gmail.com>
2025-05-18 10:33:50 +0800

    fix the bug of channel corruption probolem on admin2vin.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- vintage_proxy/src/io_object.rs
+++ vintage_proxy/src/io_object.rs
@@ -5,7 +5,7 @@ use redis::Msg;
 use serde::{Deserialize, Serialize};
 use vintage_msg::{Action, Model, Proto};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub(crate) struct InputOutputObject {
     pub action: Action,
     pub proto: Proto,
@@ -19,22 +19,31 @@ pub(crate) async fn read_msg(
     channel_name: &str,
 ) -> anyhow::Result<InputOutputObject> {
     let msg = pubsub_stream.next().await;
-    log::info!("received msg from channel {}", channel_name);
-    if msg.is_none() {
+    if let Some(msg) = msg {
+        log::info!("received msg from channel {}", channel_name);
+        let msg_payload: Vec<u8> = msg.get_payload().expect("redis msg get_payload error.");
+        let msg_obj: InputOutputObject =
+            match serde_json::from_slice::<InputOutputObject>(&msg_payload) {
+                Ok(msg_obj) => {
+                    log::info!(
+                        "from redis, msg_obj: {} {} {} {} {}",
+                        msg_obj.action,
+                        msg_obj.proto,
+                        msg_obj.model,
+                        msg_obj.data.len(),
+                        msg_obj.ext.len()
+                    );
+                    msg_obj
+                }
+                Err(_) => {
+                    log::error!("error when extract IO Object.");
+                    InputOutputObject::default()
+                }
+            };
+        Ok(msg_obj)
+    } else {
+        // if msg is None, it means the sub connection has been broken
         log::error!("received msg from channel {} is None.", channel_name);
-
-        return Err(anyhow!("msg from channel {} is None.", channel_name));
+        Err(anyhow!("msg from channel {} is None.", channel_name))
     }
-
-    let msg_payload: Vec<u8> = msg.unwrap().get_payload()?;
-    let msg_obj: InputOutputObject = serde_json::from_slice(&msg_payload).unwrap();
-    log::info!(
-        "from redis, msg_obj: {} {} {} {} {}",
-        msg_obj.action,
-        msg_obj.proto,
-        msg_obj.model,
-        msg_obj.data.len(),
-        msg_obj.ext.len()
-    );
-    Ok(msg_obj)
 }
--- vintage_proxy/src/service_admin2vin.rs
+++ vintage_proxy/src/service_admin2vin.rs
@@ -5,6 +5,7 @@ use redis::aio::PubSub;
 use serde::Deserialize;
 use std::cmp::max;
 use tokio::sync::mpsc;
+use tokio::time::{sleep, Duration};
 use vintage_msg::{MsgToBlockChain, UploadWasm};
 use vintage_utils::{SendMsg, Service};
 
@@ -35,12 +36,16 @@ impl Service for Admin2Vin {
         let mut pubsub_stream = pubsub.on_message();
 
         loop {
-            let msg_obj = read_msg(&mut pubsub_stream, ADMIN_2_VIN).await?;
-
-            if &msg_obj.action == ACTION_UPLOAD_WASM {
-                if let Err(err) = self.upload_wasm(msg_obj) {
-                    log::error!("upload wasm err: {:?}", err);
+            if let Ok(msg_obj) = read_msg(&mut pubsub_stream, ADMIN_2_VIN).await {
+                if &msg_obj.action == ACTION_UPLOAD_WASM {
+                    if let Err(err) = self.upload_wasm(msg_obj) {
+                        log::error!("upload wasm err: {:?}", err);
+                    }
                 }
+            } else {
+                // the connection has broken
+                log::error!("admin2vin channel broken");
+                sleep(Duration::from_secs(1)).await;
             }
         }
     }
@@ -50,16 +55,15 @@ impl Admin2Vin {
     fn upload_wasm(&self, object: InputOutputObject) -> anyhow::Result<()> {
         #[derive(Deserialize)]
         struct Payload {
-            wasm_file: Vec<u8>,
             sql_file: String,
             afterblocks: u64,
         }
-        let payload: Payload = serde_json::from_slice(&object.data)?;
+        let payload: Payload = serde_json::from_slice(&object.ext)?;
 
         self.blockchain_msg_sender
             .send_msg(MsgToBlockChain::UploadWasm(UploadWasm {
                 proto: object.proto,
-                wasm_binary: payload.wasm_file,
+                wasm_binary: object.data,
                 sql: payload.sql_file,
                 after_blocks: max(max(10, self.min_after_blocks), payload.afterblocks),
             }));
